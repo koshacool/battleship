4a0b1d82f56beba6be27ca92cc9a59d053cedff1
koshacool <roman.kushytskyy@gmail.com>
2019-01-04 13:42:09 +0200

    fix production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDNDvHsDyvx9j-QPAl6G9X_N38-h5utsRs',
+    authDomain: 'battleship-776a6.firebaseapp.com',
+    databaseURL: 'https://battleship-776a6.firebaseio.com',
+    projectId: 'battleship-776a6',
+  }
 };
